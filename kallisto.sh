682486253fae6da46d21f8f1de83e2089231cd86
zlemmon <zlemmon@live.com>
2018-01-31 15:27:17 -0500

    new sub method

--- kallisto.sh
+++ kallisto.sh
@@ -9,14 +9,62 @@
 # Produced by Zachary H. Lemmon on November 3, 2017. This script is not done in parallel as it is very fast and thus workflow implemented copies samples from NLSAS one at a time so as not to overwhelm I/O capacity. Also doesn't require lots of free space on HPC for copying the raw data files over as they are directly copied to the $TMPDIR and processed there.
 ###
 
-trimmomatic=$HOME/bin/Trimmomatic-0.32/trimmomatic-0.32.jar # Path to trimmomatic jar
-#kallisto_idx=$HOME/indexes/SL3.0/ITAG3.20/ITAG3.20_CDS_kallisto_index # Path to the kallisto index, make the index before run.
-KALLISTOINDEX LINE
-#samplelist=/sonas-hs/lippman/hpc/data/bop_RNAseq/Kallisto/Samples # pathtoread1	pathtoread2	samplename
-SAMPLELIST LINE
+# Set some default values
+bootstraps=0
+
+for i in "$@"; do
+	case $i in
+		--trimmomatic=*)
+		trimmomatic="${i#*=}"
+		shift
+	;;
+		--samplelist=*)
+		samplelist="${i#*=}"
+		shift
+	;;
+		--kallistoidx=*)
+		kallistoidx="${i#*=}"
+		shift
+	;;
+		--bootstraps=*)
+		bootstraps="${i#*=}"
+		shift
+	;;
+		--default)
+		DEFAULT=YES
+		shift
+	;;
+	esac
+done
+
+if [[ ! -f $trimmomatic || ! -f $samplelist || ! -f $kallistoidx ]] ; then
+	echo -ne "\nOne or more of the options supplied don't lead to actual files! Check that all are correct\n\nusage: kallisto.sh --trimmomatic=/path/to/trimmomatic.jar --samplelist=/path/to/samplelist --kallistoidx=/path/to/kallistoidx/\n\t*samplelist file is tab delimited, field 1 = read1, field 2 = read2 (or NA), field 3 = SampleName\n\n"
+
+echo "options set are:"
+echo -ne "\ttrimmomatic path : $trimmomatic\n"
+echo -ne "\tsamplelist : $samplelist\n"
+echo -ne "\tkallisto index : $kallistoidx\n"
+echo -ne "\tbootstraps : $bootstraps\n"
+
+	exit
+fi
+
+if [[ -z $TMPDIR ]] ; then TMPDIR=$PWD ; fi
+
+echo -ne "\nStarting kallisto run. options set are:\n"
+echo -ne "\ttrimmomatic path : $trimmomatic\n"
+echo -ne "\tsamplelist : $samplelist\n"
+echo -ne "\tkallisto index : $kallistoidx\n"
+echo -ne "\tbootstraps : $bootstraps\n"
+date
 
 while read read1 read2 sample rest; do
 	echo -ne "\n#########################################\n#########################################\n\nStarting sample: $sample\n\n" ;
+
+	if [[ ! -f $read1 || ! -f $read2 ]] ; then
+		echo -ne "reads in samplelist do not exist! Check the path is correct\n"
+		continue
+	fi
 
 	# Make some shorter names for the reads... cuts off everything before the last "/" of the path
 	read1short=${read1/*\/}
@@ -57,10 +105,10 @@ while read read1 read2 sample rest; do
 	# Start kallisto run... pass through if else to run the correct PE or SE command.
 	if [ $read2 != "none" ] ; then
 		echo "Starting PE kallisto run..." ; date
-		kallisto quant --index=$kallisto_idx --output-dir=$sample --threads=8 $TMPDIR/"$read1short"_P1.fastq $TMPDIR/"$read2short"_P2.fastq
+		kallisto quant --index=$kallistoidx --output-dir=$sample --threads=8 $TMPDIR/"$read1short"_P1.fastq $TMPDIR/"$read2short"_P2.fastq -b=$bootstraps
 	else
 		echo "Starting SE kallisto run..." ; date
-		kallisto quant --index=$kallisto_idx --output-dir=$sample --single --fragment-length=200 --sd=40 --threads=8 $TMPDIR/"$read1short"_P1.fastq
+		kallisto quant --index=$kallisto_idx --output-dir=$sample --single --fragment-length=200 --sd=40 --threads=8 $TMPDIR/"$read1short"_P1.fastq -b=$bootstraps
 	fi
 	echo "Done with kallisto run..." ; date
 
